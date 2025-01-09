526c0094e937c6760dad48dc57a795a58726fa0c
killian9902 <killiandempsey.tech@gmail.com>
2025-01-09 15:55:03 +0400

    Added CORS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
+
+// Configure CORS middleware
+app.use(cors({
+    origin: 'https://killians.club', // Allow requests from your frontend
+    methods: ['GET', 'POST'],       // Specify allowed HTTP methods
+    credentials: true,              // Allow credentials (cookies, etc.)
+}));
 
 // Middleware to log headers
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -9,10 +17,12 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     next();
 });
 
+// Route handler
 app.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Hello, world!' });
 });
 
+// Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
